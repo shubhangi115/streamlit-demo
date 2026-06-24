91a3b8e0f349706e82ec6cbfb687b6e425121333
shubhangi <shubhangi1152005@gmail.com>
2026-06-24 15:26:47 +0530

    created first streamlit code

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ st.title("Hello")
 st.subheader("made with streamlit")
 st.text("welcome")
 st.write("choose you fav chai")
+
+chai=st.selectbox("Your favourite chai:", {"masala chai", "lemon tea","adrak chai"})
+st.write(f"you choose {chai}, excellent choice")
+
+st.success("your chai has been brewed")
